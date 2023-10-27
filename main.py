72827e9b61d433db171a1de5c6e4dc7a6fad6c87
doyonu <doyonu@educ.college-montreal.qc.ca>
2023-10-27 16:17:32 -0400

    m

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import arcade
-import random
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+
+class MyGame(arcade.Window):
+   def __init__(self, width, height, title):
+       super().__init__(width, height, title)
+
+    def setup(self):
+        arcade.set_background_color(arcade.color.WHITE)
 
 def main():
 
-    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "tutoriel arcade")
+    window = MyGame(640, 480, "Drawing Example")
+
+    arcade.run()
+
+main()
+
+def on_draw(self):
+
+    arcade.start_render()
+
+
+    arcade.draw_circle_filled(10, 10, 20, (225, 54, 34))
